e2b2d853615e5fa5a2b6992c2606a81c2e0e07b1
L7z404 <leonelcastillo1014@gmail.com>
2020-08-20 18:42:00 -0500

    Pagina Inicial con Icono Animado

--- lib/src/pages/ingresar_codigo.dart
+++ lib/src/pages/ingresar_codigo.dart
@@ -13,39 +13,116 @@ class _IngresarCodigoPageState extends State<IngresarCodigoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Ingresar Codigo SIIAU'),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              controller: codigoController,
-              decoration: const InputDecoration(hintText: 'Ingresa Tu Codigo'),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Por favor ingresa tu codigo';
-                }
-                return null;
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: RaisedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    AlumnosApiProvider(codigoController.text);
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => HomePage(codigoController.text)));
-                  }
-                },
-                child: Text('Continuar'),
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Sistema Consultador Materias'),
         ),
-      ),
+        resizeToAvoidBottomPadding: true,
+        //
+        body: Center(
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(20.0),
+              children: [
+                Text(
+                  'Consultar Materias y Horarios',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal[300],
+                    fontSize: 18.0,
+                  ),
+                ),
+                Divider(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  maxLength: 9,
+                  autofocus: true,
+                  controller: codigoController,
+                  decoration: const InputDecoration(
+                      hintText: 'Ingresa Tu Codigo',
+                      labelText: 'Codigo*',
+                      icon: Icon(Icons.bubble_chart)),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Por favor ingresa tu codigo';
+                    }
+                    return null;
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: RaisedButton(
+                    color: Colors.teal[400],
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        AlumnosApiProvider(codigoController.text);
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) =>
+                                HomePage(codigoController.text)));
+                      }
+                    },
+                    child: Text('Continuar'),
+                  ),
+                ),
+                Divider(
+                  height: 50.0,
+                ),
+                GirarAnimacion(
+                  icon: Icons.track_changes,
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class GirarAnimacion extends StatefulWidget {
+  final IconData icon;
+  final Duration duration;
+
+  const GirarAnimacion({
+    Key key,
+    @required this.icon,
+    this.duration = const Duration(milliseconds: 1800),
+  }) : super(key: key);
+
+  @override
+  _GirarAnimacionState createState() => _GirarAnimacionState();
+}
+
+class _GirarAnimacionState extends State<GirarAnimacion>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Widget _child;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 2000),
+    )..repeat();
+    _child = Icon(
+      widget.icon,
+    );
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RotationTransition(
+      turns: _controller,
+      child: _child,
     );
   }
 }
